fd81aecf3a6b2996524c38f1fcf1e27afe4c8544
wschadow <wolfgang.schadow@t-online.de>
2021-05-08 18:51:55 +0200

    clean-up

--- Firmware/Configuration.h
+++ Firmware/Configuration.h
@@ -18,11 +18,11 @@ extern PGM_P sPrinterName;
 // Firmware version
 #define FW_VERSION "3.10.0"
 #define FW_COMMIT_NR 5260
-#define FW_COMMIT_HASH "0942fd2e"
+#define FW_COMMIT_HASH ""
 // FW_VERSION_UNKNOWN means this is an unofficial build.
 // The firmware should only be checked into github with this symbol.
-#define FW_DEV_VERSION FW_VERSION_GOLD
-#define FW_REPOSITORY "Caribou3d"
+#define FW_DEV_VERSION FW_VERSION_UNKNOWN
+#define FW_REPOSITORY "Unknown"
 #define FW_VERSION_FULL FW_VERSION "-" STR(FW_COMMIT_NR)
 
 // G-code language level
