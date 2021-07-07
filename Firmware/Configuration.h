0847abdccf38e6aa59d0264334a9046ac511f38b
wschadow <wolfgang.schadow@t-online.de>
2021-07-07 23:21:07 +0200

    updated .pot and .po files

--- Firmware/Configuration.h
+++ Firmware/Configuration.h
@@ -18,7 +18,7 @@ extern PGM_P sPrinterName;
 // Firmware version
 #define FW_VERSION "3.10.0"
 #define FW_COMMIT_NR 5263
-#define FW_COMMIT_HASH ""
+#define FW_COMMIT_HASH "9839c8fd"
 
 // FW_VERSION_UNKNOWN means this is an unofficial build.
 // The firmware should only be checked into github with this symbol.
