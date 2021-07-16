93e777d8b3994f7a764e5f94db0208fb0780d89b
wschadow <wolfgang.schadow@t-online.de>
2021-07-16 20:48:48 +0200

    updated commit no

--- Firmware/Configuration.h
+++ Firmware/Configuration.h
@@ -21,7 +21,7 @@ extern PGM_P sPrinterName;
 #define FW_REVISION 1
 #define FW_VERSION "3.10.1-RC1"
 // #define FW_VERSION STR(FW_MAJOR) "." STR(FW_MINOR) "." STR(FW_REVISION) "-RC1"
-#define FW_COMMIT_NR 5387
+#define FW_COMMIT_NR 5389
 #define FW_COMMIT_HASH "654fab87"
 
 // FW_VERSION_UNKNOWN means this is an unofficial build.
