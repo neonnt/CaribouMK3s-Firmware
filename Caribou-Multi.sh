cd1395de23bf0391840de100ad06fcb6dade8109
wschadow <wolfgang.schadow@t-online.de>
2022-04-10 23:02:23 +0200

    added scripts, updated calling sequence to PF-build.sh

--- Caribou-Multi.sh
+++ Caribou-Multi.sh
@@ -2,4 +2,4 @@
 #  Will build MK3S EN_Only final GOLD firmware 
 #  with build_number from configruatrion.h and output extra information,
 #  delete lang build temporary files, not keep Configuration_prusa..
-./PF-build.sh -d GOLD -c 1 -p 0 -o 0 -l EN_ONLY -v All
+./PF-build.sh -d GOLD -c 1 -p 0 -o 0 -l ALL
